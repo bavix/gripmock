f8b1627f13499f3e3fb1708817f39b433fb80da5
Maxim Babichev <maksim.babichev95@gmail.com>
2023-08-23 21:11:05 +0300

    optimize

---
+++ depends.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	_ "google.golang.org/grpc"
+	_ "google.golang.org/grpc/codes"
+	_ "google.golang.org/grpc/status"
+	_ "google.golang.org/protobuf/reflect/protoreflect"
+	_ "google.golang.org/protobuf/runtime/protoimpl"
+)
