f90dcb086bb0b9825b83970f1ec68b77ede29852
Maxim Babichev <maksim.babichev95@gmail.com>
2024-02-23 11:23:53 +0300

    unit FEATURE_PROTO3_OPTIONAL

--- example/ms/client/main.go
+++ example/ms/client/main.go
@@ -45,10 +45,26 @@ func main() {
 		log.Fatalf("error from grpc: %v", err)
 	}
 	log.Printf("Result: %d", r2.Code)
+
+	r3, err := c.SayHello(context.Background(), &pb.Request{V2: []string{
+		"e3484119-24e1-42d9-b4c2-7d6004ee86d9", // 1
+		"c30f45d2-f8a4-4a94-a994-4cc349bca457", // 3
+		"f1e9ed24-93ba-4e4f-ab9f-3942196d5c03", // 0
+		"cc991218-a920-40c8-9f42-3b329c8723f2", // 2
+	}, V3: int64ptr(77)})
+	if err != nil {
+		log.Fatalf("error from grpc: %v", err)
+	}
+	log.Printf("Result: %d", r3.Code)
+	log.Printf("v3: %d", *r3.V3)
 }
 
 func u2bytes(v string) []byte {
 	u := uuid.MustParse(v)
 
 	return u[:]
+}
+
+func int64ptr(v int64) *int64 {
+	return &v
 }
