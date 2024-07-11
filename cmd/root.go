7a2ba733ee4afd992aa2b64d6a903f17a400195a
Maxim Babichev <maksim.babichev95@gmail.com>
2024-07-11 20:07:25 +0300

    feat: add rest cli

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,6 @@ package cmd
 
 import (
 	"context"
-	"errors"
-	"net/http"
 	"os"
 
 	"github.com/rs/zerolog"
@@ -43,36 +41,6 @@ var rootCmd = &cobra.Command{
 
 		return builder.GRPCServe(cmd.Context(), proto.NewProtocParam(args, outputFlag, importsFlag))
 	},
-}
-
-func restServe(ctx context.Context, builder *deps.Builder) error {
-	srv, err := builder.RestServe(ctx, stubFlag)
-	if err != nil {
-		return err
-	}
-
-	ch := make(chan error)
-	defer close(ch)
-
-	go func() {
-		zerolog.Ctx(ctx).
-			Info().
-			Str("addr", builder.Config().HTTPAddr).
-			Msg("Serving stub-manager")
-
-		ch <- srv.ListenAndServe()
-	}()
-
-	select {
-	case err = <-ch:
-		if errors.Is(err, http.ErrServerClosed) {
-			return nil
-		}
-
-		return err
-	case <-ctx.Done():
-		return ctx.Err()
-	}
 }
 
 //nolint:gochecknoinits
