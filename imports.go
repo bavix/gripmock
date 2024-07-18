b8e348f8b76051957c84705ebffcb6217ee1a2f9
Maxim Babichev <maksim.babichev95@gmail.com>
2024-07-18 19:26:41 +0300

    fix deps

--- imports.go
+++ imports.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "github.com/gripmock/grpc-interceptors"
-	_ "google.golang.org/genproto/googleapis/api/annotations"
 	_ "google.golang.org/grpc/health"
 
 	_ "github.com/bavix/gripmock-sdk-go"
